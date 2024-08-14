2055a6f2eb9965ae2563160ffdc90cb7f07e7b6c
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2024-08-14 16:04:01 +0330

    refactor: add comments

--- src/ds/comparator/comparator-function.ts
+++ src/ds/comparator/comparator-function.ts
@@ -1,3 +1,14 @@
+/**
+ * Comparator function type.
+ *
+ * @param {Element} left
+ * @param {Element} right
+ * @returns {number}
+ * -1 if left is less than right.
+ * 0 if left is equal to right.
+ * 1 if left is greater than right.
+ * 
+ */
 export type ComparatorFunction<Element> = (
   left: Element,
   right: Element
