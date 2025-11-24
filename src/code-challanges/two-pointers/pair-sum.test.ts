ddb3baeca9ff03cc088a17e43947683d4bb8ae10
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2025-11-24 17:21:18 +0330

    feat: implement pair sum function with tests

---
+++ src/code-challanges/two-pointers/pair-sum.test.ts
@@ -0,0 +1,31 @@
+import { pairSumSortedBruteForce } from "./pair-sum";
+
+describe("pairSumSortedBruteForce", () => {
+  it.each([
+    {
+      input: [],
+      target: 0,
+      expected: [],
+    },
+    {
+      input: [2, 2, 3],
+      target: 5,
+      expected: [0, 2],
+    },
+    {
+      input: [-1, 2, 3],
+      target: 2,
+      expected: [0, 2],
+    },
+    {
+      input: [-3, -2, -1],
+      target: -5,
+      expected: [0, 1],
+    },
+  ])(
+    "returns $expected for input $input and target $target",
+    ({ input, target, expected }) => {
+      expect(pairSumSortedBruteForce(input, target)).toEqual(expected);
+    }
+  );
+});
